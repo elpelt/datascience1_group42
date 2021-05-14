e96e1d4d329d2dfb24174bd1db68a5c1d270bc93
Niklas Conen <niklas.conen@online.de>
2021-05-14 19:12:25 +0200

    base meta class for clustering

--- code/clustering.py
+++ code/clustering.py
@@ -1,62 +1,103 @@
 # pyclustering: https://github.com/annoviko/pyclustering/
 # documentation: https://pyclustering.github.io/docs/0.8.2/html/index.html
 # own metrics: https://github.com/annoviko/pyclustering/issues/471
-import numpy as np
 from pyclustering.utils import read_sample
-from pyclustering.cluster.kmeans import kmeans, kmeans_visualizer
-from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from pyclustering.utils.metric import type_metric, distance_metric
 from pyclustering.samples.definitions import FCPS_SAMPLES
 
-class kmeansClustering():
-    def __init__(self, path, metric):
-        self.data = read_sample(path)
-        self.metric = self.select_metric(metric)
+# datasets, maybe replace this, to reduce a dependency later
+from sklearn.datasets import load_diabetes, load_iris, load_wine
+
+
+class Clustering():
+    def __init__(self, metric, dataset, path=""):
+        self.data = []
+        self.path = path
+        self.dataset = dataset
+        self.encoding = "integer"
     
-    def select_metric(self, metric):
+    def pyc_metric(self, metric):
         # distance for plotting
-        if metric == "euclidean" or 0:
+        if metric == "euclidean":
             return distance_metric(type_metric.EUCLIDEAN)
         
-        elif metric == "manhattan" or 1:
+        elif metric == "manhattan":
             return distance_metric(type_metric.MANHATTAN)
         
-        elif metric == "chebyshev" or 2:
+        elif metric == "chebyshev":
             return distance_metric(type_metric.CHEBYSHEV)
         
-        elif metric == "mahalanobis" or 3:
+        # cosines' not converging for a centroid located at (0,0), needs fixing
+        elif metric == "cosine":
             
-            # todo
-            def mahalanobis(x, y):
-                return [(x[i]**2 + y[i]**2)**(0.5) for i in range(x)]
+            def cosine(x, y):
+                def dot(x, y):
+                    return sum([x[i]*y[i] for i in range(len(x))])
 
-            return distance_metric(type_metric.USER_DEFINED, func=mahalanobis);
+                def length(x):
+                    return dot(x, x)**0.5
+                
+                return dot(x, y)/(length(x) * length(y))
+
+            return distance_metric(type_metric.USER_DEFINED, func=cosine);
         
         else:
             print("wrong distance measure given")
             return None
-
     
+    def load_data(self):
+        """
+        loads in datasets
+        """
+        # sklearn data import
+        if self.dataset == "diabetes":
+            return load_diabetes()["data"]
+        
+        elif self.dataset == "iris":
+            return load_iris()["data"]
+        
+        elif self.dataset == "wine":
+            return load_wine()["data"]
+        
+        elif self.dataset == "solarflare":
+            return self.solar_load()
+
+    def integer_encoding(self, objects):
+        self.replacement = {}
+        objects = sorted(list(objects))
+
+        for i in range(len(objects)):
+            self.replacement[objects[i]] = i
+        
+        for i in range(len(self.data)):
+            for j in range(len(self.data[-1])):
+                if not isinstance(self.data[i][j], int):
+                    self.data[i][j] = self.replacement[self.data[i][j]]
+
+    def one_hot_encoding(self):
+        pass
+
+    def solar_load(self, skip=1):
+        groups = set()
+
+        with open(self.path, 'r') as f:
+            n = 0
+            data = [i.split(" ") for i in f.read().splitlines()[skip:]]
+
+        for i in range(len(data)):
+            for j in range(len(data[-1])):
+                try:
+                    data[i][j] = int(data[i][j])
+                except:
+                    groups.add(data[i][j])
+        
+        if self.encoding == "integer":
+            self.integer_encoding(groups)
+        
+        return data
+        
     def cluster(self):
-        # Load list of points for cluster analysis.
-        self.data = read_sample(FCPS_SAMPLES.SAMPLE_TWO_DIAMONDS)
-
-        # Prepare initial centers using K-Means++ method.
-        initial_centers = kmeans_plusplus_initializer(self.data, 2).initialize()
-
-        # Create instance of K-Means algorithm with prepared centers.
-        kmeans_instance = kmeans(self.data, initial_centers, metric=self.metric)
-
-        # Run cluster analysis and obtain results.
-        kmeans_instance.process()
-        clusters = kmeans_instance.get_clusters()
-        final_centers = kmeans_instance.get_centers()
-
-        # Visualize obtained results
-        kmeans_visualizer.show_clusters(self.data, clusters, final_centers)
-
+        pass
 
 if __name__ == "__main__":
-    path = "../datasets/shuttle/shuttle.tst"
-    test = kmeansClustering(path, "mahalanobis")
-    test.cluster()
+    pass
