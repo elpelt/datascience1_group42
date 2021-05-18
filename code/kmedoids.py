b3d8959181d0fcafe7c8038cafdefe3b57bf475e
Niklas Conen <niklas.conen@online.de>
2021-05-18 15:03:30 +0200

    labels

--- code/kmedoids.py
+++ code/kmedoids.py
@@ -3,9 +3,9 @@ from pyclustering.cluster.kmedoids import kmedoids
 from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 
 
-class kmeansClustering(Clustering):
-    def __init__(self, metric, dataset, path=""):
-        super().__init__(metric, dataset, path)
+class kmedoidsClustering(Clustering):
+    def __init__(self, metric, dataset):
+        super().__init__(metric, dataset)
         self.data = self.load_data()
         self.metric = self.pyc_metric(metric)
     
@@ -18,10 +18,16 @@ class kmeansClustering(Clustering):
             # random initialiser
             initial_medoids = random_center_initializer(self.data, k).initialize()
 
-        kmedoids_instance = kmedoids(sample, initial_medoids, metric=self.metric)
+        kmedoids_instance = kmedoids(self.data, initial_medoids, metric=self.metric)
  
         # Run cluster analysis and obtain results.
         kmedoids_instance.process()
         clusters = kmedoids_instance.get_clusters()
         medoids = kmedoids_instance.get_medoids()
         return clusters, medoids
+
+if __name__ == "__main__":
+    c = kmedoidsClustering("manhattan", "diabetis", "../datasets/solar_flares/flare.data2")
+    c.load_data()
+    print(c.cluster(4))
+    
