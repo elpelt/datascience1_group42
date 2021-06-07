0d674c753f5c3c3b9034dc7767fa864069c17576
Niklas Conen <niklas.conen@online.de>
2021-06-07 14:25:54 +0200

    brute force result calculation

---
+++ code/result_calculation.py
@@ -0,0 +1,35 @@
+from kmeans import kmeansClustering
+from kmedians import kmediansClustering
+from kmedoids import kmedoidsClustering
+from dbscan import DBSCANClustering
+
+kalgos = ['kmeans', 'kmedians', 'kmedoids']
+kalgoclass = {'kmeans': kmeansClustering, 'kmedians': kmediansClustering, 'kmedoids': kmedoidsClustering}
+
+
+distances = ["euclidean", "manhattan", "chebyshev", "cosine"]
+
+datasets = ["iris", "wine", "diabetes", "housevotes"]
+
+seed = 42
+
+results = Results("./results")
+
+for c in kalgos:
+    for d in distances:
+        for s in datasets:
+            for k in range(1, 11):
+                alg = kalgoclass[c](d, s, seed)
+                clusters, centers = alg.cluster(k)
+                results.save_set(s, c, d, clusters, centers, k=k)
+                print(f"saved {dataset}, {cluster_algo}, {cluster_dist}, k={k_value}")
+    
+    print(f"finished {c} algorithm")
+
+for d in distances:
+    for s in datasets:
+        for m in range(1, 21):
+            alg = DBSCANClustering(d, s, seed)
+            clusters, centers = alg.cluster(k)
+                results.save_set(s, c, d, clusters, centers, k=k)
+                print(f"saved {dataset}, {cluster_algo}, {cluster_dist}, minpts={minpts}, eps={epsilon}"")            
